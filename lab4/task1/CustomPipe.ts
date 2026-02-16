e23ae551b5ace912e84c6e86a9694e79ae593be8
Madina Sakpanova <madinase12@gmail.com>
2026-02-16 12:28:56 +0500

    Add files via upload

---
+++ lab4/task1/CustomPipe.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {ReversePipe} from './CustomPipe1';
+
+@Component({
+  selector: 'app-root',
+  template: ` Reverse Machine: {{ word | reverse }} `,
+  imports: [ReversePipe],
+})
+export class App {
+  word = 'You are a champion';
+}
