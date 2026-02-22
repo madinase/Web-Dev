a821292e154a20d3c87d44d33635c6d56de55b02
Madina Sakpanova <madinase12@gmail.com>
2026-02-22 23:11:19 +0500

    Delete lab4/task1 directory

--- lab4/task1/RoutingOverview2.ts
+++
@@ -1,8 +0,0 @@
-import {ApplicationConfig} from '@angular/core';
-import {provideRouter} from '@angular/router';
-
-import {routes} from './RoutiingOverview';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
-};
